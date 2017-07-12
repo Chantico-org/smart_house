0d91d815fa47ad84c405baebd2a6c445b1bd620a
Alexei Gontar <f.gontar5@gmail.com>
2017-07-12 20:24:48 +0300

    Test v0.0.1

--- server/src/test/kotlin/org/example/myhome/device_server/handlers/DeviceInteractHandlerTest.kt
+++ server/src/test/kotlin/org/example/myhome/device_server/handlers/DeviceInteractHandlerTest.kt
@@ -1,5 +1,7 @@
 package org.example.myhome.device_server.handlers
 
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import io.netty.channel.ChannelHandlerContext
 import org.example.myhome.device_server.simp.SimpMessage
 import org.example.myhome.device_server.simp.SimpMessageType
@@ -13,7 +15,7 @@ import java.util.*
 
 
 class DeviceInteractHandlerTest {
-    @Test(timeout = 10000)
+    @Test(timeout = 1000)
     fun subscribe() {
       val handler = DeviceInteractHandler()
       val context = Mockito.mock(ChannelHandlerContext::class.java)
@@ -32,21 +34,24 @@ class DeviceInteractHandlerTest {
         .verifyComplete()
     }
 
-    @Test
-    fun send() {
+  @Test(timeout = 1000)
+  fun send() {
       val handler = DeviceInteractHandler()
       val captor = ArgumentCaptor.forClass(SimpMessage::class.java)
       val context = Mockito.mock(ChannelHandlerContext::class.java)
       handler.channelRegistered(context)
       val body = UUID.randomUUID().toString()
-      StepVerifier.create(handler.send("test", ""))
+      val requestBody = UUID.randomUUID().toString()
+      StepVerifier.create(handler.send("test", requestBody))
         .then {
           Mockito.verify(context).writeAndFlush(captor.capture())
           val config = objectMapper.readTree(captor.value.body)
+          assertThat(config.hasNonNull("id"), equalTo(true))
           val message = mapOf(
             "id" to config["id"].asInt(),
             "body" to body
           )
+          assertThat(config["body"].asText(), equalTo(requestBody))
           handler.channelRead(context, SimpMessage(type = SimpMessageType.RESPONSE, body = writeValue(message)))
         }
         .expectNext(body)
