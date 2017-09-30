d1c6b59def2371b83739f7c42eced70939ea4fee
Alexei <f.gontar5@gmail.com>
2017-09-30 05:27:09 -0400

    #6 Fix of server tests

--- server/src/test/kotlin/org/example/myhome/ServerSpec.kt
+++ server/src/test/kotlin/org/example/myhome/ServerSpec.kt
@@ -1,19 +1,20 @@
 package org.example.myhome
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
 import org.example.myhome.dto.DeviceMetaDataDto
 import org.example.myhome.simp.core.SimpMessageType
 import org.junit.Test
 import java.net.Socket
 import java.util.*
 
+//TODO move it to separate module with integration tests
 class ServerSpec {
   @Test(timeout = 1000)
   fun testRegistration() {
     val socket = Socket("localhost", 7080)
-    println(socket.isConnected)
-    val input = socket.inputStream
-    val output = socket.outputStream
+    val inputStream = socket.inputStream
+    val outputStream = socket.outputStream
     val deviceMetaData = DeviceMetaDataDto(
       deviceId = UUID.randomUUID().toString(),
       deviceKey = UUID.randomUUID().toString(),
@@ -21,45 +22,12 @@ class ServerSpec {
       sensors = listOf(1, 2, 3),
       controls = listOf(0, 1, 3)
     )
-    output.writeJson(deviceMetaData, SimpMessageType.REQUEST)
-//    println(input.readSimpMessage())
-//    println(input.readSimpMessage())
-//    output.writeJson(mapOf(
-//      "topic" to "/test",
-//      "body" to "My body"
-//    ), SimpMessageType.MESSAGE)
-//    output.writeJson(mapOf(
-//      "topic" to "/test",
-//      "body" to "My body"
-//    ), SimpMessageType.MESSAGE)
-    socket.close()
-  }
-
-  @Test
-  fun testSend() {
-    val socket = Socket("localhost", 7080)
-    val objectMapper = ObjectMapper()
-    val input = socket.inputStream
-    val output = socket.outputStream
-    val deviceMetaData = DeviceMetaDataDto(
-      deviceId = UUID.randomUUID().toString(),
-      deviceKey = UUID.randomUUID().toString(),
-      firmwareVersion = 0,
-      sensors = listOf(1, 2, 3),
-      controls = listOf(0, 1, 3)
-    )
-    output.writeJson(deviceMetaData, SimpMessageType.REQUEST)
-    println(input.readSimpMessage())
-    val message = input.readSimpMessage()
-    println(message)
-    val id = objectMapper.readTree(message.body)["id"].asText()
-    println(id)
-    output.writeJson(mapOf(
-      "destination" to "/temp",
-      "id" to id,
-      "body" to "My cool body"
-    ), SimpMessageType.RESPONSE)
-
+    outputStream.writeJson(deviceMetaData, SimpMessageType.REQUEST)
+    val response = inputStream.readSimpMessage()
+    assertThat(response.type, equalTo(SimpMessageType.RESPONSE))
+    assertThat(response.body, equalTo("NO"))
+//    assert that client is disconnected
+    assertThat(inputStream.read(), equalTo(-1))
     socket.close()
   }
 }
